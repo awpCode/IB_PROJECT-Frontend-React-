386494bc4e87e3d95ba1a33cbd861bac76d998a4
awpCode <rishabh.jaypee@gmail.com>
2021-01-25 00:23:24 +0530

    new-interview-component-done

--- src/Components/NewInterview.js
+++ src/Components/NewInterview.js
@@ -8,6 +8,7 @@ const NewInterview = () => {
     const [userIds, setUserIds] = useState([]);
     const [users, setUsers] = useState([]);
     const [isAdded, setIsAdded] = useState(false);
+    const [errors, setErrors] = useState([]);
 
     useEffect(() => {
         getUsers();
@@ -28,9 +29,12 @@ const NewInterview = () => {
         try {
             res = await axios.post('http://localhost:3001/interviews',{starttime: starttime, endtime: endtime, user_ids: userIds.map(e => e.value)});
             setIsAdded(true);
+            setErrors([]);
         }
         catch(err){
-            console.log(err.response.data);
+            //console.log(err.response.data.error);
+            setErrors(err.response.data.error);
+            console.log(errors);
         }
         
     };
@@ -42,6 +46,22 @@ const NewInterview = () => {
         setEndTime(e.target.value);
     };
 
+    const printErrors = () => {
+
+        if(errors.length === 0)
+            return '';  
+        else{
+            return (
+                    <div className="alert alert-danger">
+                    <h4 className="alert-heading">The following errors prevented the Interview from getting saved.</h4>
+                    <ul>
+                        {errors.map(error => <li key = {error}>{error}</li>)}
+                    </ul>
+                     <button onClick = {() => setErrors([])} >Close</button>
+                 </div> 
+            );
+        }
+    };
     if(isAdded)
     {
         return (
@@ -51,50 +71,42 @@ const NewInterview = () => {
     return (
         <React.Fragment>
         <h1 className = "text-center"> Schedule New Interview</h1>
+        {printErrors()}        
         <div className="container m-5">
-        <div className="row justify-content-center">
-        <div className="col-10">
-        <form className = "shadow p-3 mb-5 bg-dark rounded" onSubmit = {postInterview}>
-        
-        <div className="form-group row">
-            <label className = "col-2 col-form-label text-light">Start-Time:</label>
-            <div className="col-10">
-                <input className = "form-control shadow rounded" type = "datetime-local" value = {starttime} onChange = {updateStartTime} />
-            </div>
-        </div>
-        
-        <div className="form-group row">
-            <label className = "col-2 col-form-label text-light">End-Time:</label>
-            <div className="col-10">
-                <input className = "form-control shadow rounded" type = "datetime-local" value = {endtime}  onChange = {updateEndTime} />
-            </div>
-        </div>
-        
-        <div className = "form-group row">
-            <label className = "col-2 col-form-label text-light">Users:</label>
-            <div className = "col-10">
-            <MultiSelect
-            options={users.map(user => (
-            { label: user.id,
-              value: user.id
-            }
-             ))}
-             value={userIds}
-             onChange={setUserIds}
-            labelledBy={"Select"}
-            />
-            </div>
-        </div>
+            <div className="row justify-content-center">
+                <div className="col-10">
+                    <form className = "shadow p-3 mb-5 bg-dark rounded" onSubmit = {postInterview}>
+                    
+                    <div className="form-group row">
+                        <label className = "col-2 col-form-label text-light">Start-Time:</label>
+                        <div className="col-10">
+                            <input className = "form-control shadow rounded" type = "datetime-local" value = {starttime} onChange = {updateStartTime} />
+                        </div>
+                    </div>
+                    
+                    <div className="form-group row">
+                        <label className = "col-2 col-form-label text-light">End-Time:</label>
+                        <div className="col-10">
+                            <input className = "form-control shadow rounded" type = "datetime-local" value = {endtime}  onChange = {updateEndTime} />
+                        </div>
+                    </div>
+                    
+                    <div className = "form-group row">
+                        <label className = "col-2 col-form-label text-light">Users:</label>
+                        <div className = "col-10">
+                            <MultiSelect options={users.map(user => ({ label: user.id, value: user.id}))} value={userIds} onChange={setUserIds} labelledBy={"Select"} />
+                        </div>
+                    </div>
 
-        <br />
+                    <br />
 
-        <div className="form-group row justify-content-center">
-            <button className = "btn btn-outline-light btn-lg" type = "submit" >Create Interview </button> 
-        </div>
+                    <div className="form-group row justify-content-center">
+                        <button className = "btn btn-outline-light btn-lg" type = "submit" >Create Interview </button> 
+                    </div>
 
-        </form>
-        </div>
-        </div>
+                    </form>
+                </div>
+            </div>
         </div>
         </React.Fragment>
     );
